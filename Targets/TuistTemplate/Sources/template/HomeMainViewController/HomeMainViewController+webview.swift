4e13616535c865ecacb6aab6bc83eb123081a15d
bongzniak <bongzniak@gmail.com>
2025-09-04 14:45:28 +0900

    HomeMainViewController

---
+++ Targets/TuistTemplate/Sources/template/HomeMainViewController/HomeMainViewController+webview.swift
@@ -0,0 +1,13 @@
+//
+//  HomeMainViewController.swift
+//
+//  Created by bongzniak on 9/4/25.
+//  Copyright © 2025 tuist.io. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class HomeMainViewController: UIViewController {
+    // WebView
+}
